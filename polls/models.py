0b9e8beb297f44a551f6d70b7672bfe0375b28e0
Tatpol Samakpong <nicenicegame@gmail.com>
2020-09-14 23:11:32 +0700

    Added end_date attribute in Question model

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('ending date')
 
     def __str__(self):
         return self.question_text
